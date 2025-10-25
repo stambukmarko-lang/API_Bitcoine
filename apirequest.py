17f1febbd3f41bb7132bbae2ae372e41620f9e54
stambukmarko <stambukmarko@gmail.com>
2025-10-25 21:31:22 +0200

    Create apirequest.py

---
+++ apirequest.py
@@ -0,0 +1,16 @@
+import requests
+
+url= 'https://www.ibm.com/'
+r = requests.get(url)
+r.status_code
+r.request.headers
+
+#print(r.status_code)        # ispis statusnog koda (200 = OK)
+#print(r.request.headers)    # ispis headera poslanog zahtjeva
+
+if r.status_code == 200:
+    print("Zahtjev uspješan!")
+else:
+    print(f"Greška: {r.status_code}")
+    
+print(r.text)  # ispiše prvih 500 znakova HTML-a
